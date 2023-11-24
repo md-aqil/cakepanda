2a4e7f8d36e068730a9e4d9a5512b2e23d84c229
md-aqil <aqilali381@gmail.com>
2023-11-24 01:34:45 -0800

    added

--- js/controller.js
+++ js/controller.js
@@ -1,14 +1,29 @@
-$(function() {
+// $(function() {
 	
 
-	$('.exp-section div').addClass('default');
+// 	$('.exp-section div').addClass('default');
 	
-		$('.exp-section div').on('click', function() {
+// 		$('.exp-section div').on('click', function() {
 	
-	  	var e = $('.exp-section > div');
-			if(e.hasClass('expand')){
-				 e.removeClass('expand');
-			 	$(this).addClass('expand');
-			} else { $(this).addClass('expand'); }
-		})
-})
+// 	  	var e = $('.exp-section > div');
+// 			if(e.hasClass('expand')){
+// 				 e.removeClass('expand');
+// 			 	$(this).addClass('expand');
+// 			} else { $(this).addClass('expand'); }
+// 		})
+// })
+
+$(function() {
+    $('.exp-section div').addClass('default');
+
+    $('.exp-section div').hover(
+        function() {
+            $('.exp-section > div').removeClass('expand');
+            $(this).addClass('expand');
+        },
+        function() {
+            // This function is triggered when the mouse leaves the element
+            // If you want any specific behavior on hover out, you can add it here
+        }
+    );
+});
