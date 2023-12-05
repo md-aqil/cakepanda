f9274f6c9241eaa232593149b0d5903155b9a907
md-aqil <aqilali381@gmail.com>
2023-12-05 05:36:05 -0800

    added

--- js/controller.js
+++ js/controller.js
@@ -82,7 +82,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
-  spaceBetween: 20,
+  spaceBetween: 0,
   navigation: {
     nextEl: ".swiper-button-next-unique",
     prevEl: ".swiper-button-prev-unique",
@@ -93,6 +93,8 @@ var swiper = new Swiper(".mySwiper", {
     },
   },
 });
+
+
 
 
 // particle effect 
